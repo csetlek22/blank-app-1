9ac3d65077333986cbe8fad026c99066b21c0d3e
setlekcaner <csetlek22@ku.edu.tr>
2025-07-19 15:15:11 +0300

    mmm

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,6 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 # --- CONFIGURATION ---
-import json
-
-with open("credentials.json") as f:
-    info = json.load(f)
-    print("Service account email:", info['client_email'])
 
 MODEL_NAME = "llama3-70b-8192"
 
